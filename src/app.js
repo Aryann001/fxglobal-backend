ddfdb29dbb496de6c36c43a6a572584afeec9a82
Aryan <aryanbaghel040@gmail.com>
2024-12-27 20:09:55 +0530

    2nd commit

--- src/app.js
+++ src/app.js
@@ -5,11 +5,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 // Test response
 app.get("/", (req, res) => {
