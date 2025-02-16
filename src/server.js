908e51e72daf9902f0d65fb108ec97501cda4d1f
Aryan <aryanbaghel040@gmail.com>
2025-02-16 14:46:03 +0530

    every minute cron 2nd

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
-import cron from "node-cron";
 import Finance from "./models/finance.model.js";
 
 // Config the Env file
@@ -13,7 +12,7 @@ dotenv.config({
 connectDB();
 
 // Field that updates every 24 hrs
-cron.schedule("* * * * *", async () => {
+app.get("/api/v1/run-cron", async (req, res) => {
   try {
     const finances = await Finance.find({});
 
@@ -26,8 +25,11 @@ cron.schedule("* * * * *", async () => {
         await singleUserFinance.save({ validateBeforeSave: false });
       }
     }
+
+    return res.status(200).json({ message: "Cron job executed successfully." });
   } catch (error) {
-    console.error("Error updating field:", error);
+    console.error("Error updating earned field:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
